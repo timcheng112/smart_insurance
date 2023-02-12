c37c2b5d0353b53620787c70c4beb0339111b7b0
timcheng112 <77824545+timcheng112@users.noreply.github.com>
2023-02-12 13:12:15 +0800

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,6 @@ import "firebase/compat/firestore";
 
 var firebaseConfig = {
   // Firebase credentials
-  apiKey: "AIzaSyBseo2KJ3YJDNcBBqtIhVcTR81NIycj5rQ",
-  authDomain: "smart-insurance-e89d0.firebaseapp.com",
-  databaseURL: "https://smart-insurance-e89d0-default-rtdb.firebaseio.com",
-  projectId: "smart-insurance-e89d0",
-  storageBucket: "smart-insurance-e89d0.appspot.com",
-  messagingSenderId: "534031451222",
-  appId: "1:534031451222:web:14749792b8dc422701a7f7",
-  measurementId: "G-CNVLPBZXPP",
 };
 
 // Initialize Firebase
